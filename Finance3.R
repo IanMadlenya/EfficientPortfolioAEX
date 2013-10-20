1f5a22b0622460a9f6ba540dcd2e5f4f05861789
witusj <wfth69@gmail.com>
2013-10-20 11:48:52 +0200

    Exchanged R-files

--- Finance3.R
+++ Finance3.R
@@ -1,13 +1,13 @@
 # load package quantmod
 require("quantmod")
 
-aexcomp <- read.csv("C:/Users/NB/Google Drive/Data Analysis/Finance/aexcomp1.csv")
+aexcomp <- read.csv("C:/Users/NB/Google Drive/Data Analysis/Finance/cfa.csv")
 head(aexcomp)
 
 symbols <- as.vector(aexcomp[[1]])
 
-date_begin <- as.Date("2010-01-01")
-date_end <- as.Date("2013-01-01")
+date_begin <- as.Date("2013-01-01")
+#date_end <- as.Date("")
 
 # retrieve data of all stocks
 tickers <- getSymbols(symbols, src = "yahoo", from = date_begin, auto.assign = TRUE)
@@ -18,10 +18,19 @@ for (i in 2:length(tickers)) {
 }
 
 # calculate returns
-return_lag <- 5  # (crude) weekly returns
-data_spline <- na.spline(dataset)
-data <- na.omit(ROC(data_spline, return_lag, type = "discrete"))
+# return_lag <- 5  # (crude) weekly returns
+data_spline <- na.locf(dataset)
+data <- na.omit(ROC(data_spline, type = "discrete"))
 names(data) <- symbols
+
+library("PerformanceAnalytics")
+
+wdata = window(data)
+charts.PerformanceSummary(wdata, colorset=rich6equal)
+
+png("CFAChart.png")
+charts.PerformanceSummary(wdata, colorset=rich6equal)
+dev.off()
 
 # required packages: financeR, tseries
 
@@ -45,12 +54,12 @@ frontier <- portfolioFrontier(data_ts, spec, constraints)
 print(frontier)
 
 # plot efficient frontier
-png("EffFrontAEX.png", width = 800, height = 600, units = "px", pointsize = 20)
-tailoredFrontierPlot(object = frontier, risk = "Sigma")
+png("CFAEffFront.png")
+tailoredFrontierPlot(object = frontier)
 dev.off()
 
 # plot weights
 assets <- dim(data)[2]
-png("PortWeightsAEX.png", width = 1500, height = 1500)
+png("CFAPort.png")
 weightsPlot(frontier, col = rainbow(assets))
 dev.off()
